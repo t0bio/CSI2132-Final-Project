8ff1c671353e00b4247fbfe23ac60bdb07c84961
ShawnP1337 <52176354+ShawnP1337@users.noreply.github.com>
2022-04-03 12:48:36 -0400

    Update models.py

--- DCMS/main/models.py
+++ DCMS/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    street_number = models.IntegerField()
+    street_name = models.CharField(max_length=50)
+    house_number = models.IntegerField()
+    city = models.CharField(max_length=50)
+    province = models.CharField(max_length=50)
+    ssn = models.CharField(max_length=50)
+    gender = models.CharField(max_length=25)
+    email_address = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
