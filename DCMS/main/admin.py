266f2d5fc125b402b41f8da39820fb75c05dec10
ShawnP1337 <52176354+ShawnP1337@users.noreply.github.com>
2022-04-03 20:40:57 -0400

    Some stuff changed
    asdadasd

--- DCMS/main/admin.py
+++ DCMS/main/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import User,   Employee
+from .models import User,Employee,Patient,Appointment, Appointment_Procedure, Review, Treatment, Patient_billing, Patient_record, Fee_Charge, Insurance_claim, Invoice
+
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Employee)
+admin.site.register(Patient)
+admin.site.register(Appointment)
+admin.site.register(Appointment_Procedure)
+admin.site.register(Review)
+admin.site.register(Treatment)
+admin.site.register(Patient_billing)
+admin.site.register(Patient_record)
+admin.site.register(Fee_Charge)
+admin.site.register(Insurance_claim)
+admin.site.register(Invoice)
--- DCMS/main/models.py
+++ DCMS/main/models.py
@@ -16,6 +16,8 @@ class User(models.Model):
     gender = models.CharField(max_length=25)
     email_address = models.CharField(max_length=50)
     date_of_birth = models.DateField()
+
+    
     
 class Patient(models.Model):
     patient_id = models.IntegerField(primary_key=True)
@@ -23,11 +25,17 @@ class Patient(models.Model):
     health_card_no = models.IntegerField()
     user_id = models.ForeignKey('User', on_delete=models.CASCADE)
 
+    
+
 class Employee(models.Model):
     employee_id = models.IntegerField(primary_key=True)
     employee_type = models.CharField(max_length=50)
     salary = models.IntegerField()
     user_id = models.ForeignKey('User', on_delete = models.CASCADE)
+    
+    
+
+    
 
 class Appointment(models.Model):
     appointment_id = models.IntegerField(primary_key = True)
@@ -39,6 +47,8 @@ class Appointment(models.Model):
     room_assigned = models.IntegerField()
     employee_id = models.ForeignKey('Employee', on_delete=models.CASCADE)
     patient_id = models.ForeignKey('Patient', on_delete=models.CASCADE)
+
+    
 
 class Appointment_Procedure(models.Model):
     procedure_id = models.IntegerField(primary_key=True,null=False)
@@ -56,6 +66,8 @@ class Appointment_Procedure(models.Model):
     bill_id = models.IntegerField()
     appointment_id = models.ForeignKey('Appointment', on_delete=models.CASCADE)
 
+    
+
 class Review(models.Model):
     review_id = models.IntegerField(primary_key=True)
     communication = models.CharField(max_length=500)
@@ -63,12 +75,15 @@ class Review(models.Model):
     cleanliness = models.CharField(max_length=500)
     value = models.IntegerField()
     patient_id = models.ForeignKey('Patient', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
     
 class Treatment(models.Model):
     treatment_id = models.IntegerField(primary_key=True)
     treatment_type = models.CharField(max_length=20)
-    appointment_type = models.ForeignKey('Appointment', on_delete=models.CASCADE)  
-    
+    appointment_type = models.ForeignKey('Appointment', on_delete=models.CASCADE)
+   
 class Patient_record(models.Model):
     patient_record_id = models.IntegerField(primary_key=True)
     patient_id = models.ForeignKey('Patient', on_delete=models.CASCADE)
@@ -77,12 +92,15 @@ class Patient_record(models.Model):
     tooth = models.CharField(max_length=100)
     comments = models.CharField(max_length=100)
     treatment_id = models.ForeignKey('Treatment', on_delete=models.CASCADE)
+    
 
 class Insurance_claim(models.Model):
     claim_id = models.IntegerField(primary_key=True)
     claim_amount = models.FloatField()
     insurance_company = models.CharField(max_length=50)
-  
+    
+    
+   
 class Invoice(models.Model):
     invoice_id = models.IntegerField(primary_key=True)
     patient_id = models.ForeignKey('Patient', on_delete=models.CASCADE)
@@ -93,7 +111,7 @@ class Invoice(models.Model):
     discount = models.FloatField()
     penalty = models.FloatField()
     insurance_claim_id = models.ForeignKey('Insurance_claim', on_delete=models.CASCADE)
-
+   
 class Patient_billing(models.Model):
      bill_id = models.IntegerField(primary_key=True)
      appointment_id = models.ForeignKey('Appointment', on_delete = models.CASCADE)
@@ -102,10 +120,12 @@ class Patient_billing(models.Model):
      insurance_claim_id = models.ForeignKey('Insurance_claim', on_delete = models.CASCADE)
      payment_type = models.CharField(max_length = 50)
      total_amount = models.FloatField()
-
+     
 class Fee_Charge(models.Model):
     feed_id = models.IntegerField(primary_key=True)
     procedure_id = models.IntegerField()
     fee_code = models.IntegerField()
     charge = models.IntegerField()
     procedure_id = models.ForeignKey('Appointment_Procedure',on_delete=models.CASCADE)
+    
+    
