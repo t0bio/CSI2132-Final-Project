735285b344fa97401a85e0255f650823745493a6
ShawnP1337 <52176354+ShawnP1337@users.noreply.github.com>
2022-04-08 14:19:08 -0400

    Fixed createAccount method

--- DCMS/authentication/views.py
+++ DCMS/authentication/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def createAccount(response):
-    form = UserCreationForm
+    if response.method =="POST":
+        form = UserCreationForm(response.POST)
+
+        if form.is_valid():
+            form.save()
+
+        return redirect('receptionist')
+    else:
+        form = UserCreationForm()
+    
     return render(response, "authentication/createaccount.html", {"form":form})
